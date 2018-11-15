52549f9009481c1e885913ea6124bcbec5105d34
potapenkom <potapenko05121985@gmail.com>
2018-11-15 20:12:33 +0200

    second commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,18 @@
-$('.owl-carousel').owlCarousel({
+$('#carousel__icon').owlCarousel({
     loop:true,
     margin:0,
     nav:true,
+    navText:["<i class='fas fa-angle-double-left'></i>","<i class='fas fa-angle-double-right'></i>"],
     responsiveClass:true,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:3,
-            nav:true
+            items:3
         },
         1000:{
-            items:4,
-            nav:true
+            items:4
         }
     }
 })
