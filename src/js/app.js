ab857498104f9ecb28b7570b96ef846a817c2bc3
potapenkom <potapenko05121985@gmail.com>
2018-11-17 10:18:45 +0200

    all project

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,7 @@
-$('#carousel__icon').owlCarousel({
+$('.owl-carousel').owlCarousel({
     loop:true,
     margin:0,
-    nav:true,
-    navText:["<i class='fas fa-angle-double-left'></i>","<i class='fas fa-angle-double-right'></i>"],
+    items:4,
     responsiveClass:true,
     responsive:{
         0:{
@@ -16,3 +15,37 @@ $('#carousel__icon').owlCarousel({
         }
     }
 })
+
+$(document).ready(function(){
+    var $carousel = $(".owl-carousel"),
+        $left = $(".carousel__prew"),
+        $right = $(".carousel__next")
+    
+    $carousel.owlCarousel();
+    $right.click(function() {
+      $carousel.trigger('next.owl.carousel',500);
+    });
+    $left.click(function() {
+      $carousel.trigger('prev.owl.carousel',500);
+    }); 
+    console.log($carousel)
+  });
+
+
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId            : 'fb-root',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v3.2'
+    });
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));  
